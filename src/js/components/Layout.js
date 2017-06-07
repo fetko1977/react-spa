2b63c1e98e72fac5583343ba4e4236366da176aa
Svetoslav Savov <svetoslav.savov@stanga.net>
2017-06-08 00:02:04 +0300

    Added bootstrap, react-router and removed unnecesery components.

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -6,19 +6,12 @@ import Header from "./Header";
 export default class Layout extends React.Component {
   constructor() {
     super();
-    this.state = {
-      title: "Welcome",
-    };
-  }
-
-  changeTitle(title) {
-    this.setState({title});
   }
 
   render() {
     return (
-      <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+      <div className="app-wrapper">
+        <Header />
         <Footer />
       </div>
     );
